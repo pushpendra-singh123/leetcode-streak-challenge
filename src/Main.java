54fab91639028b6d8e985bc4fe0954a1d1e9ed99
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-06-04 08:53:26 +0530

    take input characters and store it in array.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
+
+//    static void reverse(char[] arr){
+//        int start = 0;
+//        int end = arr.length - 1;
+//        while (start<end){
+//            char temp = arr[start];
+//            arr[start] = arr[end];
+//            arr[end] = temp;
+//            start++;
+//            end--;
+//        }
+//    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
--- src/strings/ReverseString_344.java
+++ src/strings/ReverseString_344.java
@@ -1,22 +1,30 @@
 package strings;
 
+import java.util.Scanner;
+
 public class ReverseString_344 {
     public static void reverseString(char[] s) {
-        int start =0;
-        int end = s.length -1;
-        while(start<end){
+        int start = 0;
+        int end = s.length - 1;
+        while (start < end) {
             char temp = s[start];
             s[start] = s[end];
             s[end] = temp;
-            start ++;
-            end --;
+            start++;
+            end--;
         }
     }
 
     public static void main(String[] args) {
-        char[] arr = {'a','b','c','d'};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        char[] arr = new char[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.next().charAt(0);
+        }
         reverseString(arr);
-        for (char c : arr){
+        for (char c : arr) {
             System.out.print(c);
         }
     }
