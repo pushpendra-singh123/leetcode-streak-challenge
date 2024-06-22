2ef947a913be5cebb814ff3b2ecd328fe391ef4c
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-06-22 07:17:35 +0530

    By Engineering Digest. Easy to solve.

---
+++ src/CountSubArray_1248.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+// Solution by Engineering Digest.
+
+public class CountSubArray_1248 {
+    public static int numberOfSubarrays(int[] nums, int k) {
+        int res = 0, curr = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for (int e : nums){
+            curr += e%2;
+            res += map.getOrDefault(curr -k, 0);
+            map.put(curr,map.getOrDefault(curr,0)+1);
+        }
+        return res;
+    }
+}
