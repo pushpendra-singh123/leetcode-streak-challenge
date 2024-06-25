608773090bf461aa481d3b9d1ab97ab044d4db51
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-06-25 22:40:52 +0530

    AlgorithmHQ

---
+++ src/BSTGreaterSum_1038.java
@@ -0,0 +1,18 @@
+import javax.swing.tree.TreeNode;
+
+public class BSTGreaterSum_1038 {
+
+//    public static TreeNode bstToGst(TreeNode root) {
+//        solve(root,0);
+//        return root;
+//
+//    }
+//    private static int solve(TreeNode root, int sum){
+//        if (root == null) return 0;
+//        int right = solve(root.right, sum);
+//        int val = root.val;
+//        root.val = val + sum + right;
+//        int left = solve(root.left,root.val);
+//        return right+left+val;
+//    }
+}
