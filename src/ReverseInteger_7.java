703711181f137831dbc0f148dddadb471e084bd1
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-19 21:58:42 +0530

    lucky no in matrix

---
+++ src/ReverseInteger_7.java
@@ -0,0 +1,17 @@
+public class ReverseInteger_7 {
+    public static int reverse(int x) {
+//        if (x<0){
+//            x = x * (-1);
+//            StringBuilder str = new StringBuilder(String.valueOf(x));
+//            return (-1) * (int)Integer.parseInt(str.reverse().toString());
+//        }
+//        StringBuilder str = new StringBuilder(x);
+//        return (int)Integer.parseInt(str.reverse().toString());
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-321));
+    }
+}
---
+++ src/arrays/LuckyNo_1380.java
@@ -0,0 +1,27 @@
+package arrays;
+import java.util.*;
+public class LuckyNo_1380 {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int maxOfRowMins = Integer.MIN_VALUE;
+        for (int i = 0; i < rows; i++) {
+            int minVal = matrix[i][0];
+            for (int j = 0; j < cols; j++) {
+                minVal = Math.min(minVal,matrix[i][j]);
+            }
+            maxOfRowMins = Math.max(minVal, maxOfRowMins);
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < cols; i++) {
+            int maxVal = matrix[0][i];
+            for (int j = 0; j < rows; j++) {
+                maxVal = Math.max(maxVal, matrix[j][i]);
+            }
+            if (maxVal == maxOfRowMins){
+                res.add(maxVal);
+            }
+        }
+        return res;
+    }
+}
