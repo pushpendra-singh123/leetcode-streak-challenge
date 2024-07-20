5b9283495760a37185f9da259cb23d59192bd3d3
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-20 23:28:38 +0530

    matrix easy

---
+++ src/arrays/ValidMatrix_1605.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class ValidMatrix_1605 {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int rows = rowSum.length;
+        int cols = colSum.length;
+        int r = 0;
+        int c = 0;
+        int[][] resMatrix = new int[rows][cols];
+        while (r < rows && c < cols) {
+            int minVal = Math.min(rowSum[r], colSum[c]);
+            resMatrix[r][c] = minVal;
+            rowSum[r] -= minVal;
+            colSum[c] -= minVal;
+            if (rowSum[r] == 0) {
+                r++;
+            } else {
+                c++;
+            }
+        }
+        return resMatrix;
+    }
+}
