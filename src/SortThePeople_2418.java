475306c620d3479caa83ef1ea925064cca9fecd4
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2024-07-22 21:19:39 +0530

    not so easy problem "shash"

---
+++ src/SortThePeople_2418.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortThePeople_2418 {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = heights.length;
+        Integer index[] = new Integer[n];
+        //ON
+        for(Integer i=0;i<n;i++){
+            index[i] = i;
+        }
+        //NlogN
+        //merge sort, quick sort ....
+        Arrays.sort(index, new Comparator<Integer>(){
+            public int compare(Integer a, Integer b){
+                return heights[b] - heights[a];
+            }
+        });
+        String res[] = new String[n];
+        int i=0;
+        // On
+        for(Integer ind : index){
+            res[i] = names[ind];
+            i++;
+        }
+        return res;
+
+    }
+}
